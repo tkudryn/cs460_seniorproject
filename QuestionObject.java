6cf78759c66cdc82d9aa4128b23edacbdc2ac269
tkudryn <62193912+tkudryn@users.noreply.github.com>
2020-11-10 11:28:19 -1000

    Add files via upload

---
+++ QuestionObject.java
@@ -0,0 +1,36 @@
+package flash_card_code;
+
+import java.lang.*;
+
+public class QuestionObject {
+    private String term;
+    private String definition;
+
+    //default constructor
+    QuestionObject()
+    {
+        term = null;
+        definition = null;
+    }
+
+    //overwritten constructor
+    QuestionObject(String uiTerm, String uiDefinition)
+    {
+        term = uiTerm;
+        definition = uiDefinition;
+    }
+
+    // general getters or accessors
+
+    public String getTerm() {
+
+        return term;
+    }
+
+    public String getDefinition(){
+
+        return definition;
+    }
+
+}
+
